83862a521f26bfed970438b2911468848cc01c58
Koshara Aleksandr <alex.koshara1990@gmail.com>
2024-01-19 23:03:58 +0800

    initial app

---
+++ services/api.services.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
+
+const getIcon = (icon) => {
+  switch (icon.slice(0, -1)) {
+    case '01':
+      return '🌦';
+    case '02':
+      return '☂';
+    case '03':
+      return '☔';
+    case '04':
+      return '❅';
+    case '09':
+      return '🌈';
+    case '10':
+      return '';
+    case '11':
+      return '🌊';
+    case '13':
+      return '🌩';
+    case '50':
+      return '🌬';
+    
+  }
+}
+
+const getWeather = async (city) => {
+  const token = process.env.token ?? await getKeyValue(TOKEN_DICTIONARY.token);
+
+  if (!token) {
+    throw new Error('Need key API. Use -t [API_KEY]');
+  }
+
+  const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather',
+    {
+      params: {
+        q: city,
+        appid: token,
+        lang: 'ru',
+        units: 'metric'
+      }
+    }  
+  )
+
+  return data;
+};
+
+export {
+  getWeather,
+  getIcon
+};
---
+++ weather.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+import { log } from 'console';
+import { getArgs } from './helpers/args.js';
+import { getWeather, getIcon } from './services/api.services.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.services.js';
+import { TOKEN_DICTIONARY, getKeyValue, saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError('No founded token');
+    return;
+  }
+
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token)
+    printSuccess('Token saved');
+  } catch(e) {
+    printError(e.message);
+  }
+};
+
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError('Please write city');
+    return;
+  }
+
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city)
+    printSuccess('City saved');
+  } catch (e) {
+    printError(e.message);
+  }
+};
+
+const getForcast = async () => {
+  try {
+    const city = process.env.city ?? await getKeyValue(TOKEN_DICTIONARY.city)
+    const weather = await getWeather(city);
+    printWeather(weather, getIcon(weather.weather[0].icon));
+  } catch(e) {
+    if (e?.response?.status == 404) {
+      printError('Unknow city');
+    } else if (e?.response?.status == 401) {
+      printError('Unknow token')
+    } else {
+      printError(e.message);
+    }
+  }
+};
+
+const initCLI = () => {
+  const args = getArgs(process.argv);
+
+  if (args.h) {
+    printHelp();
+  }
+
+  if (args.s) {
+    return saveCity(args.s)
+  }
+
+  if (args.t) {
+    return saveToken(args.t)
+  }
+  return getForcast();
+};
+
+initCLI();
